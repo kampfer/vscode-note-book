58bde766ff38bea97167bb79d3f199ac46f4490b
liaowei <l.w.kampfer@gmail.com>
2020-06-11 17:26:35 +0800

    update

--- src/extension.js
+++ src/extension.js
@@ -1,26 +1,28 @@
 // https://code.visualstudio.com/api/extension-guides/tree-view
 
 const vscode = require('vscode');
-const { walk } = require('./utils');
 const path = require('path');
 const fs = require('fs');
+const { walk } = require('./utils');
+const Tags = require('./Tags');
+const Notes = require('./Notes');
 
+const extensionName = 'vscode-note-book';
 const cwd = vscode.workspace.workspaceFolders[0];
 const fsPath = cwd.uri.fsPath;  // 工作目录的地址
+const localStoragePath = path.join(fsPath, '.note_books');
 
-function activate(context) {
+const commands = [{
+    id: `${extensionName}.init`,
+    fn: function () {
 
-    if (!cwd) return;
-
-    // https://code.visualstudio.com/api/extension-guides/command#creating-new-commands
-    let disposable = vscode.commands.registerCommand('vscode-note-book.init', function () {
-
-        const localStoragePath = path.join(fsPath, '.note_books');
-        const existsNoteBooks = fs.existsSync(localStoragePath);
+        let existsNoteBooks = fs.existsSync(localStoragePath);
 
         if (!existsNoteBooks) {
 
             fs.mkdirSync(localStoragePath);
+            fs.openSync(path.join(localStoragePath, 'notes.json'), '[]');
+            fs.openSync(path.join(localStoragePath, 'tags.json'), '[]');
 
         } else {
 
@@ -28,9 +30,27 @@ function activate(context) {
 
         }
 
-    });
+    }
+}, {
+    id: `${extensionName}.createTag`,
+    fn: function (name) {
+        tags.createTag(name);
+    }
+}]
 
-    context.subscriptions.push(disposable);
+function activate(context) {
+
+    if (!cwd) return;
+
+    // https://code.visualstudio.com/api/extension-guides/command#creating-new-commands
+    for(let command of commands) {
+
+        let disposable = vscode.commands.registerCommand(command.id, command.fn);
+
+        context.subscriptions.push(disposable);
+
+    }
+
 }
 
 // this method is called when your extension is deactivated
