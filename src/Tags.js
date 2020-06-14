443ba2f3b6f577d77fcbab0bb4d2bc26531e31f5
liaowei <l.w.kampfer@gmail.com>
2020-06-14 16:52:03 +0800

    提取tags

--- src/Tags.js
+++ src/Tags.js
@@ -1,5 +1,159 @@
-module.exports = class Tags {
+const fs = require('fs');
 
-    constructor() {}
+class Tags {
+
+    constructor(storePath) {
+
+        this._storePath = storePath;
+
+        /**
+         * 数据格式：
+         * [
+         *      {
+         *          tagName: 'test',
+         *          notes: [
+         *              'notePath1',
+         *              'notePath2',
+         *          ],
+         *      },
+         *      {
+         *          tagName: 'test',
+         *          notes: [
+         *              'notePath1',
+         *              'notePath2',
+         *          ],
+         *      }
+         *      ...
+         * ]
+         */
+        if (storePath) {
+
+            this._tags = JSON.parse(fs.readFileSync(storePath));
+
+        } else {
+
+            this._tags = [];
+
+        }
+
+
+    }
+
+    extractTagsFromDocument(document) {
+
+        let documentContent = document.getText(),
+            tagsContent = documentContent.match(Tags.testReg),
+            tagsOfDocument;
+
+        if (tagsContent) tagsOfDocument = tagsContent[0].split('\n').filter((v, i, arr) => i !== 0 && i !== arr.length - 1);
+
+        if (tagsOfDocument) tagsOfDocument.forEach(tagName => this.addTagToNote(tagName, document.uri.fsPath));
+
+    }
+
+    hasTag(tagName) {
+
+        let tags = this._tags;
+
+        for (let i = 0, l = tags.length; i < l; i++) {
+
+            if (tags[i].name === tagName) return true;
+
+        }
+
+        return false;
+
+    }
+
+    hasNote(tagName, note) {
+
+        let tag = this.getTag(tagName);
+
+        if (tag) {
+
+            let notes = tag.notes,
+                hasNote = false;
+
+            for (let i = 0, l = notes.length; i < l; i++) {
+
+                if (notes[i].name === note.name) hasNote = true;
+
+            }
+
+            return hasNote;
+
+        }
+
+        return false;
+
+    }
+
+    createTag(tagName, notes = []) {
+        this._tags.push({ tagName, notes });
+    }
+
+    getTag(tagName) {
+
+        let tags = this._tags;
+
+        for (let i = 0, l = tags.length; i < l; i++) {
+
+            if (tags[i].tagName === tagName) return tags[i];
+
+        }
+
+        return null;
+
+    }
+
+    addTagToNote(tagName, notePath) {
+
+        let tag = this.getTag(tagName),
+            needUpdate = false;
+
+        if (tag) {
+
+            let notes = tag.notes,
+                hasNote = false;
+
+            for (let i = 0, l = notes.length; i < l; i++) {
+
+                if (notes[i] === notePath) hasNote = true;
+
+            }
+
+            if (hasNote === false) {
+
+                notes.push(notePath);
+
+                needUpdate = true;
+
+            }
+
+        } else {
+
+            this.createTag(tagName, [notePath]);
+
+            needUpdate = true;
+
+        }
+
+        if (needUpdate) this.store();
+
+        console.log(needUpdate);
+
+    }
+
+    store() {
+
+        clearTimeout(this._timer);
+
+        this._timer = setTimeout(() => fs.writeFileSync(this._storePath, JSON.stringify(this._tags)), 50);
+
+    }
 
 }
+
+Tags.testReg = /\\begin\{tags\}([\s\w*]*)\\end\{tags\}/;
+
+module.exports = Tags;
--- src/extension.js
+++ src/extension.js
@@ -8,42 +8,61 @@ const Tags = require('./Tags');
 const Notes = require('./Notes');
 
 const extensionName = 'vscode-note-book';
-const cwd = vscode.workspace.workspaceFolders[0];
-const fsPath = cwd.uri.fsPath;  // 工作目录的地址
-const localStoragePath = path.join(fsPath, '.note_books');
-
-const commands = [{
-    id: `${extensionName}.init`,
-    fn: function () {
-
-        let existsNoteBooks = fs.existsSync(localStoragePath);
-
-        if (!existsNoteBooks) {
-
-            fs.mkdirSync(localStoragePath);
-            fs.openSync(path.join(localStoragePath, 'notes.json'), '[]');
-            fs.openSync(path.join(localStoragePath, 'tags.json'), '[]');
-
-        } else {
-
-            vscode.window.showInformationMessage('./.note_books已存在，不需要重复初始化。');
-
-        }
-
-    }
-}, {
-    id: `${extensionName}.createTag`,
-    fn: function (name) {
-        tags.createTag(name);
-    }
-}]
 
 function activate(context) {
 
+    let cwd = vscode.workspace.workspaceFolders[0];
+
     if (!cwd) return;
 
+    let fsPath = cwd.uri.fsPath;  // 工作目录的地址
+    let localStoragePath = path.join(fsPath, '.note_books');
+    let tagsStoragePath = path.join(localStoragePath, 'tags.json');
+    let notesStoragePath = path.join(localStoragePath, 'notes.json');
+
+    const tags = new Tags(tagsStoragePath);
+    const notes = new Notes(notesStoragePath);
+
+    const commands = [
+        {
+            id: `${extensionName}.init`,
+            fn: function () {
+
+                let existsNoteBooks = fs.existsSync(localStoragePath);
+
+                if (!existsNoteBooks) {
+
+                    fs.mkdirSync(localStoragePath);
+                    fs.openSync(path.join(localStoragePath, 'notes.json'), '[]');
+                    fs.openSync(path.join(localStoragePath, 'tags.json'), '[]');
+
+                } else {
+
+                    vscode.window.showInformationMessage('./.note_books已存在，不需要重复初始化。');
+
+                }
+
+            }
+        }
+    ];
+
+    vscode.workspace.onDidChangeWorkspaceFolders(function () {
+        console.log('onDidChangeWorkspaceFolders');
+    });
+
+    vscode.workspace.onDidSaveTextDocument(function () {
+
+        let textEditor = vscode.window.activeTextEditor,
+            document = textEditor.document;
+
+        tags.extractTagsFromDocument(document);
+
+        console.log('onDidSaveTextDocument');
+
+    });
+
     // https://code.visualstudio.com/api/extension-guides/command#creating-new-commands
-    for(let command of commands) {
+    for (let command of commands) {
 
         let disposable = vscode.commands.registerCommand(command.id, command.fn);
 
