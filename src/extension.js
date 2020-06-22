9b5f674db4b01b2c3a13aa07e5f6a56839d8f028
liaowei <l.w.kampfer@gmail.com>
2020-06-22 21:53:37 +0800

    duplex link

--- src/extension.js
+++ src/extension.js
@@ -56,7 +56,66 @@ function activate(context) {
 
     return {
         extendMarkdownIt(md) {
-            return md.use(require('markdown-it-codepen'));
+            return md.use(require('markdown-it-codepen'))
+                // 参考markdown-it link规则实现`[[xxx]]`语法
+                // https://github.com/markdown-it/markdown-it/blob/master/lib/rules_inline/link.js
+                .use(function (md) {
+
+                    md.inline.ruler.before('emphasis', 'duplexLink', function (state, silent) {
+
+                        if (silent) return false;
+
+                        let content = state.src;
+
+                        // 0x5b === '['
+                        if (content.charCodeAt(state.pos) !== 0x5b) return false;
+
+                        let labelStart = state.pos + 2,
+                            labelEnd = labelStart;
+
+                        while(labelEnd < state.posMax) {
+
+                            // 0x5d === ']'
+                            if (content.charCodeAt(labelEnd) === 0x5d && content.charCodeAt(labelEnd + 1) === 0x5d) {
+                                labelEnd++;
+                                break;
+                            }
+
+                            labelEnd++;
+
+                        }
+
+                        if (labelEnd < 0 || labelEnd > state.posMax) return false;
+
+                        let title = content.substring(labelStart, labelEnd - 1),
+                            href = `${title}.md`,
+                            token;
+
+                        // <a href="somePath">title</a>
+                        // ^^^^^^^^^^^^^^^^^^^
+                        token = state.push('link_open', 'a', 1);
+                        token.attrs = [
+                            ['href', href],
+                            ['title', title]
+                        ];
+
+                        // <a href="somePath">title</a>
+                        //                    ^^^^^
+                        token = state.push('text', '', 0);
+                        token.content = title;
+
+                        // <a href="somePath">title</a>
+                        //                         ^^^^
+                        token = state.push('link_close', 'a', -1);
+
+                        // markdown-it从新pos开始继续分析剩下文档内容
+                        state.pos = labelEnd + 1;
+
+                        return true;
+
+                    });
+
+                });
         }
     };
 
