8693d99b1c6527180a2fae28addcf575c9e885a3
liaowei <liaowei@cestc.cn>
2020-12-11 17:09:39 +0800

    抽取networkgraph

--- src/renderer/notebook/index.js
+++ src/renderer/notebook/index.js
@@ -1,187 +1,23 @@
 // document.body.innerHTML = (new Date()).toString();
 
-import * as d3 from 'd3';
+import NetworkGraph from './NetworkGraph';
+
+import './graph.css';
 
 const vscode = acquireVsCodeApi();
 
-const types = ['downLink', 'upLink'];
-const color = d3.scaleOrdinal(types, d3.schemeCategory10);
-const height = window.innerHeight;
-const width = window.innerWidth;
-const normalColor = color('downLink');
-const activeColor = color('upLink');
+const graph = new NetworkGraph({
+    container: 'body',
+    width: window.innerWidth,
+    height: window.innerHeight
+});
 
-let rootElem, linkElems, nodeElems, circleElems, links, nodes;
-
-const drag = simulation => {
-
-    function dragstarted(e, d) {
-        if (!e.active) simulation.alphaTarget(0.3).restart();
-        d.fx = d.x;
-        d.fy = d.y;
+NetworkGraph.registerNode('note', {
+    create(selection, graph) {
+        const nodeSelection = selection.append('circle');
+        return nodeSelection;
     }
-
-    function dragged(e, d) {
-        d.fx = e.x;
-        d.fy = e.y;
-    }
-
-    function dragended(e, d) {
-        if (!e.active) simulation.alphaTarget(0);
-        d.fx = null;
-        d.fy = null;
-    }
-
-    return d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended);
-}
-
-function linkArc(d) {
-    const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
-    return `
-      M${d.source.x},${d.source.y}
-      A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
-    `;
-}
-
-// TODO 效率不高，待优化
-function findRelatedNodesAndLinks(links, id) {
-
-    function find(links, id, direction, result) {
-
-        for (let i = 0, l = links.length; i < l; i++) {
-
-            let link = links[i];
-
-            if (direction === 'up') {
-
-                if (link.target.id === id && result.relatedLinks.indexOf(i) < 0) {
-
-                    result.relatedLinks.push(i);
-                    find(links, link.source.id, direction, result);
-
-                }
-
-            } else if (direction === 'down') {
-
-                if (link.source.id === id && result.relatedLinks.indexOf(i) < 0) {
-
-                    result.relatedLinks.push(i);
-                    find(links, link.target.id, direction, result);
-
-                }
-
-            }
-
-        }
-
-        if (result.relatedNodes.indexOf(id) < 0) result.relatedNodes.push(id);
-
-    }
-
-    const result = {
-        relatedNodes: [],
-        relatedLinks: []
-    };
-
-    if (id) {
-        find(links, id, 'up', result);
-        find(links, id, 'down', result);
-    }
-
-    return result;
-
-}
-
-function renderNoteBook(data) {
-
-    links = data.links;
-    nodes = data.nodes;
-
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id))
-        .force("charge", d3.forceManyBody().strength(-400))
-        .force("x", d3.forceX())
-        .force("y", d3.forceY());
-
-    const svg = d3.create("svg")
-        .attr("viewBox", [-width / 2, -height / 2, width, height])
-        .style("font", "12px sans-serif");
-
-    // Per-type markers, as they don't inherit styles.
-    svg.append("defs").selectAll("marker")
-        .data(types)
-        .join("marker")
-        .attr("id", d => `arrow-${d}`)
-        .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 15)
-        .attr("refY", -0.5)
-        .attr("markerWidth", 6)
-        .attr("markerHeight", 6)
-        .attr("orient", "auto")
-        .append("path")
-        .attr("fill", color)
-        .attr("d", "M0,-5L10,0L0,5");
-
-    const link = svg.append("g")
-        .attr("fill", "none")
-        .attr("stroke-width", 1.5)
-        .selectAll("path")
-        .data(links)
-        .join("path")
-        .attr("stroke", d => color(d.type))
-        .attr("marker-end", d => `url(${new URL(`#arrow-${d.type}`, location)})`);
-
-    const node = svg.append("g")
-        .attr("fill", "currentColor")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-linejoin", "round")
-        .selectAll("g")
-        .data(nodes)
-        .join("g")
-        .call(drag(simulation));
-
-    node.on('click', (e, { id }) => {
-
-        vscode.postMessage({
-            command: 'selectNote',
-            data: { id }
-        });
-
-        e.preventDefault();
-        e.stopPropagation();
-
-    });
-
-    const circles = node.append("circle")
-        .attr("stroke", "white")
-        .attr("stroke-width", 1.5)
-        .attr("r", 4);
-
-    node.append("text")
-        .attr("x", 8)
-        .attr("y", "0.31em")
-        .text(d => d.id);
-
-    simulation.on("tick", () => {
-        link.attr("d", linkArc);
-        node.attr("transform", d => `translate(${d.x},${d.y})`);
-    });
-
-    rootElem = svg;
-    linkElems = link;
-    nodeElems = node;
-    circleElems = circles;
-
-    let state = vscode.getState();
-
-    if (state && state.currentNode) selectNode(state.currentNode);
-
-    document.body.appendChild(rootElem.node());
-
-}
+});
 
 function selectNode(id) {
 
@@ -217,12 +53,13 @@ window.addEventListener('message', event => {
     } else if (message.command === 'selectNote') {
         selectNode(message.data.id);
     } else {
-        renderNoteBook(message);
+        const data = { nodes: message.nodes, edges: message.links };
+        graph.render(data);
     }
 
 });
 
-window.addEventListener('resize', () => resizeView());
+// window.addEventListener('resize', () => resizeView());
 
 vscode.postMessage({
     command: 'getGraphDataOfNoteBook'
